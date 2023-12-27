28da2a6c84219ecddbbf104b295b225c447d07c8
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-27 11:22:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+let table1=document.getElementById("sampleTable");
 function insert_Row() {
     //Write your code here
-  
-  
+	// backside insert
+	
+	// let tr=document.createElement("tr");
+	// let td1=document.createElement("td");
+	// let td2=document.createElement("td");
+	// td1.innerText="New Cell1";
+	// td2.innerText="New Cell2";
+	// tr.append(td1,td2);
+	// table1.appendChild(tr);
+  let newRow=table1.insertRow(0);
+	let td1=newRow.insertCell(0);
+	let td2=newRow.insertCell(1);
+	td1.innerText="New Cell1";
+	td2.innerText="New Cell2";
 }
